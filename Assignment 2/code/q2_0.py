fe9587a9a170f98e691ddf7512cae100c04e63a1
Gokul Kumar Kaushik <gokul.kmr@live.in>
2017-11-09 22:04:09 -0500

    Completed Q2-0

--- Assignment 2/code/q2_0.py
+++ Assignment 2/code/q2_0.py
@@ -10,17 +10,26 @@ import numpy as np
 # Import pyplot - plt.imshow is useful!
 import matplotlib.pyplot as plt
 
+IMAGE_SIZE = 8
+
 def plot_means(train_data, train_labels):
     means = []
     for i in range(0, 10):
         i_digits = data.get_digits_by_label(train_data, train_labels, i)
         # Compute mean of class i
-        average = np.mean(i_digits,axis=0)
-        means.append(average)
+
+        # Take means for each feature for all samples
+        verticalMeans = np.mean(i_digits,axis=0)
+
+        # Put the average back to an 8-by-8 pixel array
+        verticalMeans = verticalMeans.reshape(IMAGE_SIZE,IMAGE_SIZE)
+
+        # Add them to the means array
+        means.append(verticalMeans)
 
     means=np.asarray(means)
     # Plot all means on same axis
-    all_concat = np.concatenate(means, 0)
+    all_concat = np.concatenate(means, 1)
     plt.imshow(all_concat, cmap='gray')
     plt.show()
 
