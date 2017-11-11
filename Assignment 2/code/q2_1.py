a6caa1fcdb0198aa228173a42ea9136e3948b3b0
Gokul Kumar Kaushik <gokul.kmr@live.in>
2017-11-10 21:56:16 -0500

    Q2-1 part 1 done.

--- Assignment 2/code/q2_1.py
+++ Assignment 2/code/q2_1.py
@@ -38,13 +38,54 @@ class KNearestNeighbor(object):
         return np.squeeze(dist)
 
     def query_knn(self, test_point, k):
-        '''
-        Query a single test point using the k-NN algorithm
 
-        You should return the digit label provided by the algorithm
-        '''
-        digit = None
+        # Query a single test point using the k-NN algorithm
+
+        # Get the distances from self to every test point in the dataset
+        distances = self.l2_distance(test_point)
+
+        # Sort the distances to find the points with least distance -> greatest distance
+        ordered_indices = np.argsort(distances)
+
+        # Populate k nearest neighbor inside an array
+        k_nearest_digits = []
+
+        # Iterate for upto k times and add the nearest digit value
+        for i in range (0,k):
+            # Get the index from the order
+            nearest_neighbor_index = ordered_indices[i]
+
+            # Get the nearest neighbor digit for the index
+            nearest_neighbor_digit= self.train_labels[nearest_neighbor_index]
+
+            # Push the nearest neighbor digit into the array
+            k_nearest_digits.append(nearest_neighbor_digit)
+
+        # Decide what the answer is by using calculating the most frequent digit among the k neighbors
+        digit = self.digit_decider(k_nearest_digits, k)
+
         return digit
+
+    def digit_decider(self, k_nearest_digits, k):
+
+        # Obtain the unique neighbors list and their frequency (counts) -- sorted
+        unique_digits, unique_indices, unique_digit_counts = np.unique(k_nearest_digits, return_counts=True,
+                                                                return_inverse = True)
+        # Find the most frequent neighbor
+        maximum_digit_count = np.amax(unique_digit_counts)
+
+        # Check if there are more values that are as frequent (i.e. is there a tie ?)
+        number_of_maximum_frequency_occurences = list(unique_digit_counts).count(maximum_digit_count)
+
+        # If there happens to be a a tie
+        if number_of_maximum_frequency_occurences != 1:
+            # Perform the analysis again for the last neighbor removed
+            return self.digit_decider(k_nearest_digits[:k-1],k-1)
+
+        # If there is no tie, return the unique digit with the highest frequency count
+        else:
+            return unique_digits[np.argmax(np.bincount(unique_indices))]
+
 
 def cross_validation(train_data, train_labels, k_range=np.arange(1,16)):
     '''
@@ -65,14 +106,31 @@ def classification_accuracy(knn, k, eval_data, eval_labels):
     Evaluate the classification accuracy of knn on the given 'eval_data'
     using the labels
     '''
-    pass
+
+    number_of_evaluated_samples = 0
+    number_of_correctly_evaluated_samples = 0
+
+    for i in range (len(eval_data)):
+
+        # Make a prediction for the given data sample
+        prediction = knn.query_knn(eval_data[i],k)
+
+        # If your answer is correct, add to the correct count
+        if prediction == eval_labels[i]:
+            number_of_correctly_evaluated_samples += 1
+
+        # Increase the evaulated samples count
+        number_of_evaluated_samples +=1
+
+    return (number_of_correctly_evaluated_samples/number_of_evaluated_samples)
 
 def main():
     train_data, train_labels, test_data, test_labels = data.load_all_data('data')
     knn = KNearestNeighbor(train_data, train_labels)
 
-    # Example usage:
-    predicted_label = knn.query_knn(test_data[0], 1)
+    a = classification_accuracy(knn, 15, test_data, test_labels)
+    print(a)
+
 
 if __name__ == '__main__':
     main()
